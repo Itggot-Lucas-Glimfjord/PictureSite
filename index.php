ab63c51ff1bf00697bf293642ecbca077827e25e
Itggot-Lucas-Glimfjord <lucas.glimfjord@itggot.se>
2015-11-24 14:38:54 +0100

    More changes in school

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <link href="css/unsemantic-grid-responsive-tablet.css" rel="stylesheet" type="text/css"/>
         
         <script src="js/jquery.js" type="text/javascript"></script>
+		<script src="js/jquery-ui.js" type="text/javascript"></script>
         <script src="js/javascript.js" type="text/javascript"></script>
         
 	</head>
@@ -14,57 +15,87 @@
 		<div id="wrapper">
 			
             <header>
-                <nav>
-                    <section id="upload">
-                        <p>Upload</p>
-                    </section>
-                </nav>
+            	<nav>
+					<ul class="listmenu">
+						<li id="upload">Upload</li>
+					</ul>
+				</nav>
             </header>
-            
-            <section id="uploadForm">
-                <form method="post" action="index.php" enctype="multipart/form-data">
-                    <p>
-                        <input type="file" name="upImage">
-                    </p>
-                    <p>
-                        <input type="submit" name="submit" value="Upload Image">
-                    </p>
-                </form>
-				    <?php
-	$img_dir = "img/uploads/";
-    if(isset($_POST["submit"]) && $_POST["submit"] === "Upload Image") {
+			
+			<div id="position_displacer" class="grid-100"></div>
+			<div id="uploadForm">
+					<form method="post" action="index.php" enctype="multipart/form-data">
+						<p>
+							<input type="file" name="upImage">
+						</p>
+						<p>
+							<input type="submit" name="submit" value="Upload Image">
+						</p>
+					</form>
+				
+			</div>
 
-		$target_file = $img_dir . basename($_FILES["upImage"]["name"]);
-		$validIMG = 1;
-		
-		getimagesize($_FILES["upImage"]["name"]);
-		if( file_exists($target_file) ){
-			$validIMG = 0;
-		}
-		
-		// Tries to move the uploaded img file away from the tmpfolder if it's a valid img
-		if($validIMG === 1 && move_uploaded_file($_FILES["upImage"]["tmp_name"], $target_file)){}
-	}
-    ?>
-            </section>
-            
 			<?php
+				if(isset($_POST["submit"]) && $_POST["submit"] === "Upload Image") {
+					$img_dir = "img/uploads/";
+					$target_file = $img_dir . basename($_FILES["upImage"]["name"]);
+					$validIMG = 1;
+
+					// Tries to move the uploaded img file away from the tmpfolder if it's a valid img
+					if($validIMG === 1 && move_uploaded_file($_FILES["upImage"]["tmp_name"], $target_file)){}
+				}
+			
+				$img_dir = "img/uploads/";
 				$uploadedIMG = scandir($img_dir);
 				array_shift($uploadedIMG);
 				array_shift($uploadedIMG);
-				
-				foreach($uploadedIMG as $img){
+			
+			
 			?>
-				<div class="image">
-					<img src=<?php echo "\"" . $img_dir . $img . "\""; ?>>
-				</div>
-			<?php
-				}
+			
+            <section class="prefix-5 grid-30 30 row">
+				<?php
+					for($i=0;$i+2<count($uploadedIMG);$i=$i+3){
+				?>
+						
+					<article class="uploadedIMGArticle">
+						<img src="<?php echo $img_dir . $uploadedIMG[$i]; ?>"/>
+					</article>
+				<?php
+					}
+				?>
 				
+            </section> 
+            <section class="grid-30 30 row">
+				<?php
+					for($i=1;$i+2<count($uploadedIMG);$i=$i+3){
+				?>
+						
+					<article class="uploadedIMGArticle">
+						<img src="<?php echo $img_dir . $uploadedIMG[$i]; ?>"/>
+					</article>
+				<?php
+					}
+				?>
 				
+            </section> 
+            <section class="grid-30 30 row">
+				<?php
+					for($i=2;$i+2<count($uploadedIMG);$i=$i+3){
+				?>
+						
+					<article class="uploadedIMGArticle">
+						<img src="<?php echo $img_dir . $uploadedIMG[$i]; ?>"/>
+					</article>
+				<?php
+					}
+				?>
 				
-            ?>
+            </section>
+			   
         </div>
     </body>
 
-</html>
+</html>    
+
+
--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,12 @@
 
-
 $(document).ready(function () {	
     $("#uploadForm").hide();
     
 	$("#upload").click(function(){
-        $("#uploadForm").slideToggle();    
+	    $("#uploadForm").slideToggle();
+		$(".30").switchClass("grid-30 30", "grid-25 25");
+		$(".25").switchClass("grid-25 25", "grid-30 30");
+
     
     });
 
